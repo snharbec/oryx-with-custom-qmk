ea1d9049ebe7574ff55b2204ef4494a9b3897126
snharbec <66774677+snharbec@users.noreply.github.com>
2025-01-14 19:06:31 +0100

    Update achordion.c

--- VPpAr/features/achordion.c
+++ VPpAr/features/achordion.c
@@ -335,11 +335,6 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
                                            keyrecord_t* tap_hold_record,
                                            uint16_t other_keycode,
                                            keyrecord_t* other_record) {
-  // So if we are on a different row than allow hold function
-  if (tap_hold_record->event.key.row != other_record->event.key.row)
-    return true;
-  if (tap_hold_record->event.key.col == 1)
-    return true;
   return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
