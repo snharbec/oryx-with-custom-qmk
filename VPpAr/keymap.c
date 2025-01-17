1ebcff09e04f20e2da7113b96290f6af253efd93
Stefan Harbeck <stefan@harbeck.name>
2025-01-17 10:59:58 +0100

    Activate debug information

--- VPpAr/keymap.c
+++ VPpAr/keymap.c
@@ -123,6 +123,8 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 extern rgb_config_t rgb_matrix_config;
 
 void keyboard_post_init_user(void) {
+  debug_enable = true;
+  debug_matrix = true;
   rgb_matrix_enable();
 }
 
