3f681a64ad6e4271196a34aed35af873f89e8d2a
Stefan Harbeck <stefan@harbeck.name>
2025-01-17 11:10:23 +0100

    Modified logging

--- VPpAr/features/achordion.c
+++ VPpAr/features/achordion.c
@@ -336,7 +336,8 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
                                            uint16_t other_keycode,
                                            keyrecord_t* other_record) {
    // So if we are on a different row than allow hold function
-    dprintf("Achordion: Col %d, Row %d.", tap_hold_record->event.key.col, tap_hold_record->event.key.row);
+  dprintln("HAR");
+  dprintf("Achordion: Col %d, Row %d\n", tap_hold_record->event.key.col, tap_hold_record->event.key.row);
   if (tap_hold_record->event.key.col == 0)
     return true;
   // if (tap_hold_record->event.key.row != other_record->event.key.row)
